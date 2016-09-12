496f84bc9d9bfd8becb0d7ae180dae0b7956bb48
Robin Millette <robin@millette.info>
2016-09-12 13:04:27 -0400

    Upgrade deps.

--- test.js
+++ test.js
@@ -1,11 +1,10 @@
-/*eslint arrow-parens: [2, "as-needed"]*/
 'use strict'
 import test from 'ava'
 import fn from './'
 
 test('acc', async t => {
   let store = await fn('tremblay')
-  t.is(Object.keys(store).length, 171)
+  t.is(Object.keys(store).length, 176)
   store = await fn('veronique', store)
-  t.is(Object.keys(store).length, 74)
+  t.is(Object.keys(store).length, 82)
 })
