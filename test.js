5b8866197adaf5fcaf8d1fc3f9a5db2a904f38fa
Robin Millette <robin@millette.info>
2016-09-16 15:31:15 -0400

    Fix test (brittle, I knw, I know...).

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ import fn from './'
 
 test('acc', async t => {
   let store = await fn('tremblay')
-  t.is(Object.keys(store).length, 176)
+  t.is(Object.keys(store).length, 177)
   store = await fn('veronique', store)
   t.is(Object.keys(store).length, 82)
 })
