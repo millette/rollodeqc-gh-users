aba97cfc1bb1af5bfe48f537c5ab6b697bf8385e
Robin Millette <robin@millette.info>
2017-01-26 15:45:44 -0500

    Fix brittle tests.

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ import fn from './'
 
 test('acc', async t => {
   let store = await fn('tremblay')
-  t.is(Object.keys(store).length, 182)
+  t.is(Object.keys(store).length, 190)
   store = await fn('veronique', store)
-  t.is(Object.keys(store).length, 90)
+  t.is(Object.keys(store).length, 95)
 })
